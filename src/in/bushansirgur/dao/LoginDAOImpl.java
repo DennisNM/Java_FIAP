3e867cb26da1b3bb9d55d8323d46fc7acc0d37f9
Dennis <santossouzade@gmail.com>
2023-11-10 21:32:13 -0300

    commit inicial

--- src/in/bushansirgur/dao/LoginDAOImpl.java
+++ src/in/bushansirgur/dao/LoginDAOImpl.java
@@ -3,6 +3,7 @@ package in.bushansirgur.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import in.bushansirgur.model.Login;
 import in.bushansirgur.util.DBConnectionUtil;
@@ -11,20 +12,31 @@ public class LoginDAOImpl implements LoginDAO{
 
 	@Override
 	public String loginCheck(Login loginBean) {
-		String query="select * from tbl_login where email=? and password=?";
-		
+		String query="select * from tbl_login where email = " + "'"+ loginBean.getEmail() +"'" + " and password = " + "'" + loginBean.getPassword() + "'";
+		System.out.println(query);
 		try{
 			Connection con=DBConnectionUtil.openConnection();
-			PreparedStatement ps=con.prepareStatement(query);
-			ps.setString(1,loginBean.getEmail());
-			ps.setString(2,loginBean.getPassword());
+		//	PreparedStatement ps=con.prepareStatement(query);
 			
-			ResultSet rs=ps.executeQuery();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+	
 			
-			if(rs.next()){
+			System.out.println(con);
+			String checkEmail = "";
+			//ResultSet rs=ps.executeQuery();
+			while(rs.next()){
+				System.out.println(rs.getString("EMAIL"));
+				checkEmail = rs.getString("EMAIL");
+			}
+		System.out.println(checkEmail);
+			System.out.println(loginBean.getEmail() + loginBean.getPassword());
+			if(checkEmail != null && checkEmail != ""){
+				System.out.println("true");
 				return "true";
 			}
 			else{
+				System.out.println("false");
 				return "false";
 			}
 		}catch(Exception e){
--- src/in/bushansirgur/util/DBConnectionUtil.java
+++ src/in/bushansirgur/util/DBConnectionUtil.java
@@ -8,13 +8,13 @@ import java.sql.Statement;
 
 public class DBConnectionUtil {
 	
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String URL = "thin:@oracle.fiap.com.br:1521:ORCL";
 	
 	private static final String DRIVER = "oracle.jdbc.OracleDriver";
 	
-	private static final String USERNAME = "fullstack";
+	private static final String USERNAME = "rm552297";
 	
-	private static final String PASSWORD = "Swami1498";
+	private static final String PASSWORD = "100103";
 	
 	private static Connection connection = null;
 	
@@ -23,9 +23,11 @@ public class DBConnectionUtil {
             return connection;
         else {
             try {
+            	
                 Class.forName(DRIVER);
-                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            } catch (ClassNotFoundException e) {
+             connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm552297", "100103");
+               // connection = DriverManager.getConnection(URL,USERNAME ,PASSWORD);
+            } catch ( ClassNotFoundException e) {
                 e.printStackTrace();
             } catch (SQLException e) {
                 e.printStackTrace();
